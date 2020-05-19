0f64ccf9bf6d8ea87733f1bb2cc8150645ef3ab3
Orestis Lampridis <lorestis@csd.auth.gr>
2020-05-20 02:14:09 +0300

    Fixes

--- app.py
+++ app.py
@@ -8,10 +8,12 @@ from ColorDescriptor import ColorDescriptor
 from Searcher import Searcher
 from ShapeDescriptor import ShapeDescriptor
 
-app = Flask(__name__, static_url_path='static')
+app = Flask(__name__, static_url_path='/static')
 
-# db_URL = 'postgres://lwonnzirfqjlrj:239cab6b982fbb869cbb8ca219e068622bbe0c3bb05da6c06af5837659e6bcec@ec2-46-137-177-160.eu-west-1.compute.amazonaws.com:5432/d9a5legl875uce'
-db_URL = os.environ.get('DATABASE_URL')
+db_URL = 'postgres://lwonnzirfqjlrj:239cab6b982fbb869cbb8ca219e068622bbe0c3bb05da6c06af5837659e6bcec@ec2-46-137-177-160.eu-west-1.compute.amazonaws.com:5432/d9a5legl875uce'
+
+
+# db_URL = os.environ.get('DATABASE_URL')
 
 
 # main route
@@ -112,6 +114,7 @@ def send_image(filename):
     print(relative_path)
     # return send_from_directory("/static/image", relative_path)
     return filename
+
 
 if __name__ == "__main__":
     app.run(debug=True)
